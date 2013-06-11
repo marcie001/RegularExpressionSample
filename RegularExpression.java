f3d16f482c214ba9354ca835efa4d12f1f01f795
marcie001 <marcie00001@gmail.com>
2013-06-11 16:20:02 +0900

    前回マッチの末尾のサンプル追加

--- RegularExpression.java
+++ RegularExpression.java
@@ -9,14 +9,26 @@ public class RegularExpression {
             System.out.println(mt.group(1));
         }
 
-        System.out.println("");
+        System.out.println("--------");
 
         String s1 = "    郵便番号    :100-0001です";
         String p1 = "(?:\\s*郵便番号\\s*:|\\G-)(\\d+)";
-        Pattern pt1 = Pattern.compile(p1, Pattern.MULTILINE);
+        Pattern pt1 = Pattern.compile(p1);
         Matcher mt1 = pt1.matcher(s1);
         while (mt1.find()) {
             System.out.println(mt1.group(1));
+            System.out.println(mt1.group());
+        }
+
+        System.out.println("--------");
+
+        String s2 = "    郵便番号    :   100      -   0001 です";
+        String p2 = "(?:\\s*郵便番号\\s*:\\s*|\\G\\s*-\\s*)(\\d+)";
+        Pattern pt2 = Pattern.compile(p2);
+        Matcher mt2 = pt2.matcher(s2);
+        while (mt2.find()) {
+            System.out.println(mt2.group(1));
+            System.out.println(mt2.group());
         }
     }
 }
